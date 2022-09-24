6f65f8ca7c0d8b3e1772390281779ce05623de7f
Man Nguku <110129154+mannguku@users.noreply.github.com>
2022-09-24 22:07:13 +0000

    added sentry files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+	sentry: {
+    disableServerWebpackPlugin: true,
+    disableClientWebpackPlugin: true,
+  },
 }
 
 module.exports = nextConfig
